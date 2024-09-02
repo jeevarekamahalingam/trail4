c2591f325de206f22d9790e5661305aad202dd7b
jeevareka296@gmail.com <jeevareka296@gmail.com>
2024-09-02 15:24:06 +0530

    two files added using add --all functionality

---
+++ first.js
@@ -0,0 +1,34 @@
+let score=prompt("Enter your score :");
+if(score>=90 &&score<=100) console.log("Your grade is A");
+else if(score>=70 &&score<=89) console.log("Your grade is B");
+else if(score>=60 &&score<=69) console.log("Your grade is C");
+else if(score>=50 &&score<=59) console.log("Your grade is D");
+else console.log("Your grade is F");
+
+
+// for(let i=0;i<200;i++){
+//     console.log("Hello");
+// }
+
+// let Name=prompt("Enter your name:");
+// let user_name="@"+Name.trim().replaceAll(/\s+/g,"").toLowerCase()+Name.trim().length;
+// console.log(user_name);
+
+// let arr=[1,2,3,4,5,6,6];
+// console.log(arr.splice(3));
+// console.log(arr);
+
+
+// let arr=["Bloomberg","Microsoft","Uber","IBM","Netflix"];
+// arr.unshift();
+// arr.splice(2,1,"Ola");
+// arr.push("Amazon");
+// console.log(arr);
+
+// const check_vowel (s)=>{
+
+//     let ar=['a','','i','o','u'];
+//     for(let el of ar){
+//         for(let )
+//     }
+// };
